d79885fdc37b709dead7cbf9b6f5a40a434693bf
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-20 15:56:42 -0400

    part2

---
+++ Mobile/Covid-Locker/source/koltin/Unit.java
@@ -0,0 +1,14 @@
+package kotlin;
+
+@Metadata(mo6272bv = {1, 0, 3}, mo6273d1 = {"\u0000\u0010\n\u0002\u0010\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0000\bÆ\u0002\u0018\u00002\u00020\u0001B\u0002\b\u0002J\b\u0010\u0002\u001a\u00020\u0003H\u0016¨\u0006\u0004"}, mo6274d2 = {"", "", "toString", "", "kotlin-stdlib"}, mo6275k = 1, mo6276mv = {1, 1, 15})
+/* compiled from: Unit.kt */
+public final class Unit {
+    public static final Unit INSTANCE = new Unit();
+
+    public String toString() {
+        return "kotlin.Unit";
+    }
+
+    private Unit() {
+    }
+}
