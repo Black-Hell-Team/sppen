622011269965d3e67dd231324c35677e8ed5c828
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-20 17:32:48 -0400

    Add files via upload

---
+++ Mobile/Covid-Locker/source/androidx/arch/core/executor/DefaultTaskExecutor.java
@@ -0,0 +1,46 @@
+package androidx.arch.core.executor;
+
+import android.os.Handler;
+import android.os.Looper;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DefaultTaskExecutor extends TaskExecutor {
+    private final ExecutorService mDiskIO = Executors.newFixedThreadPool(2, new ThreadFactory() {
+        /* class androidx.arch.core.executor.DefaultTaskExecutor.ThreadFactoryC01031 */
+        private static final String THREAD_NAME_STEM = "arch_disk_io_%d";
+        private final AtomicInteger mThreadId = new AtomicInteger(0);
+
+        public Thread newThread(Runnable runnable) {
+            Thread thread = new Thread(runnable);
+            thread.setName(String.format(THREAD_NAME_STEM, Integer.valueOf(this.mThreadId.getAndIncrement())));
+            return thread;
+        }
+    });
+    private final Object mLock = new Object();
+    private volatile Handler mMainHandler;
+
+    @Override // androidx.arch.core.executor.TaskExecutor
+    public void executeOnDiskIO(Runnable runnable) {
+        this.mDiskIO.execute(runnable);
+    }
+
+    @Override // androidx.arch.core.executor.TaskExecutor
+    public void postToMainThread(Runnable runnable) {
+        if (this.mMainHandler == null) {
+            synchronized (this.mLock) {
+                if (this.mMainHandler == null) {
+                    this.mMainHandler = new Handler(Looper.getMainLooper());
+                }
+            }
+        }
+        this.mMainHandler.post(runnable);
+    }
+
+    @Override // androidx.arch.core.executor.TaskExecutor
+    public boolean isMainThread() {
+        return Looper.getMainLooper().getThread() == Thread.currentThread();
+    }
+}
