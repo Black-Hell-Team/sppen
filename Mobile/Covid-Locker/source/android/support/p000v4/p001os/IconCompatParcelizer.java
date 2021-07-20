bbe55cfbbeee6901a1160ec4d1575e29f9dfce12
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-20 16:28:02 -0400

    Add files via upload

---
+++ Mobile/Covid-Locker/source/android/support/p000v4/p001os/IconCompatParcelizer.java
@@ -0,0 +1,15 @@
+package android.support.p000v4.graphics.drawable;
+
+import androidx.core.graphics.drawable.IconCompat;
+import androidx.versionedparcelable.VersionedParcel;
+
+/* renamed from: android.support.v4.graphics.drawable.IconCompatParcelizer */
+public final class IconCompatParcelizer extends androidx.core.graphics.drawable.IconCompatParcelizer {
+    public static IconCompat read(VersionedParcel versionedParcel) {
+        return androidx.core.graphics.drawable.IconCompatParcelizer.read(versionedParcel);
+    }
+
+    public static void write(IconCompat iconCompat, VersionedParcel versionedParcel) {
+        androidx.core.graphics.drawable.IconCompatParcelizer.write(iconCompat, versionedParcel);
+    }
+}
