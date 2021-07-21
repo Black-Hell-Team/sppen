3aff07c828908ced2a1dd8f6e42d946ec24d679f
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-20 21:12:19 -0400

    Add files via upload

---
+++ Mobile/Covid-Locker/source/koltin/jvm/internal/unsafe/monitor.java
@@ -0,0 +1,15 @@
+package kotlin.jvm.internal.unsafe;
+
+import kotlin.Metadata;
+
+@Metadata(mo6272bv = {1, 0, 3}, mo6273d1 = {"\u0000\u0010\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u0000\n\u0002\b\u0002\u001a\u0010\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u0003H\u0002\u001a\u0010\u0010\u0004\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u0003H\u0002¨\u0006\u0005"}, mo6274d2 = {"monitorEnter", "", "monitor", "", "monitorExit", "kotlin-stdlib"}, mo6275k = 2, mo6276mv = {1, 1, 15})
+/* renamed from: kotlin.jvm.internal.unsafe.MonitorKt */
+public final class monitor {
+    private static final void monitorEnter(Object obj) {
+        throw new UnsupportedOperationException("This function can only be used privately");
+    }
+
+    private static final void monitorExit(Object obj) {
+        throw new UnsupportedOperationException("This function can only be used privately");
+    }
+}
