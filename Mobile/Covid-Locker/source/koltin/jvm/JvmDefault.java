6f9a81da11526d58b7e855dfea13cef531f89a80
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-20 21:06:16 -0400

    Add files via upload

---
+++ Mobile/Covid-Locker/source/koltin/jvm/JvmDefault.java
@@ -0,0 +1,16 @@
+package kotlin.jvm;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import kotlin.Metadata;
+import kotlin.annotation.AnnotationTarget;
+
+@Target({ElementType.METHOD})
+@Metadata(mo6272bv = {1, 0, 3}, mo6273d1 = {"\u0000\n\n\u0002\u0018\u0002\n\u0002\u0010\u001b\n\u0000\b\u0002\u0018\u00002\u00020\u0001B\u0000ø\u0001\u0000\u0002\u0007\n\u0005\b(0\u0001¨\u0006\u0002"}, mo6274d2 = {"Lkotlin/jvm/JvmDefault;", "", "kotlin-stdlib"}, mo6275k = 1, mo6276mv = {1, 1, 15})
+@kotlin.annotation.Target(allowedTargets = {AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY})
+@Retention(RetentionPolicy.RUNTIME)
+/* compiled from: JvmDefault.kt */
+public @interface JvmDefault {
+}
