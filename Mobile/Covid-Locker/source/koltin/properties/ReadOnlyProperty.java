2e437efecb8f5782e0ce772c89240ed1fb6505d8
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-20 21:27:55 -0400

    Add files via upload

---
+++ Mobile/Covid-Locker/source/koltin/properties/ReadOnlyProperty.java
@@ -0,0 +1,10 @@
+package kotlin.properties;
+
+import kotlin.Metadata;
+import kotlin.reflect.KProperty;
+
+@Metadata(mo6272bv = {1, 0, 3}, mo6273d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0000\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\bf\u0018\u0000*\u0006\b\u0000\u0010\u0001 \u0000*\u0006\b\u0001\u0010\u0002 \u00012\u00020\u0003J\"\u0010\u0004\u001a\u00028\u00012\u0006\u0010\u0005\u001a\u00028\u00002\n\u0010\u0006\u001a\u0006\u0012\u0002\b\u00030\u0007H¦\u0002¢\u0006\u0002\u0010\b¨\u0006\t"}, mo6274d2 = {"Lkotlin/properties/ReadOnlyProperty;", "R", "T", "", "getValue", "thisRef", "property", "Lkotlin/reflect/KProperty;", "(Ljava/lang/Object;Lkotlin/reflect/KProperty;)Ljava/lang/Object;", "kotlin-stdlib"}, mo6275k = 1, mo6276mv = {1, 1, 15})
+/* compiled from: Interfaces.kt */
+public interface ReadOnlyProperty<R, T> {
+    T getValue(R r, KProperty<?> kProperty);
+}
